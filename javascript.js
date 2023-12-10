d4ba9dfd4133ac844fd4fca737e3867e23af0898
yaraAawad <awadyara2000@gmail.com>
2023-12-10 12:19:29 +0300

    JS Calculator
    initial

---
+++ javascript.js
@@ -0,0 +1,87 @@
+function buttonMod() {
+
+}
+function buttonLB() {
+
+}
+function buttonRB() {
+
+}
+function buttonDel() {
+
+}
+function buttonAC() {
+
+}
+
+
+
+function buttonSin() {
+
+}
+function button7() {
+
+}
+function button8() {
+
+}
+function button9() {
+
+}
+function buttondiv() {
+
+}
+
+
+
+
+function buttonCos() {
+
+}
+
+function button4() {
+
+}
+function button5() {
+
+}
+function button6() {
+
+}
+function buttonMul() {
+
+}
+
+
+
+function buttonTan() {
+
+}
+function button1() {
+
+}
+function button2() {
+
+}
+function button3() {
+
+}
+function buttonMinus() {
+
+}
+
+
+
+
+function buttonPower() {
+
+}
+function button0() {
+
+}
+function buttonEqual() {
+
+}
+function buttonPlus() {
+
+}
